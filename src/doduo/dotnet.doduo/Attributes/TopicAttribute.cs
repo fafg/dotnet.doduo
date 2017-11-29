53677090a543439ccee76a423bfd743ad9b8ecfc
Marcelo Ferreira <marcelo.ferreira@farfetch.com>
2017-11-29 14:54:03 +0000

    Create attributes to subiscrible

---
+++ src/doduo/dotnet.doduo/Attributes/TopicAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnet.doduo.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public abstract class DoduoTopicAttribute : Attribute
+    {
+        public DoduoTopicAttribute(string topic)
+        {
+            Topic = topic;
+        }
+
+        public string Topic { get; set; }
+    }
+}
